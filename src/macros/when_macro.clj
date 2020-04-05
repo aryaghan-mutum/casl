d8c7ff12e32c53cc38dce09ee01fa23aeca3ea67
anurag <anurag.muthyam@rocketmail.com>
2020-04-04 21:48:31 -0400

    added divison

--- src/macros/when_macro.clj
+++ src/macros/when_macro.clj
@@ -1,16 +1,13 @@
 (ns macros.when_macro)
 
-
 ;; Examples:
-
 (defmacro when [x & body]
-  {:added "1.0"}
   (println (list 'if x (cons 'do body))))
 
 (when (= 2 (+ 1 1))
       (print "you got")
       (print " the touch!")
-      (println))                                             ;(if (= 2 (+ 1 1)) (do (print you got) (print  the touch!) (println)))
+      (println))                                                     ;(if (= 2 (+ 1 1)) (do (print you got) (print  the touch!) (println)))
 
 (when clauses
       (list 'if (first clauses)
@@ -18,8 +15,13 @@
               (second clauses)
               (throw (IllegalArgumentException.
                        "cond requires an even number of forms")))
-            (cons 'clojure.core/cond (next (next clauses))))) ;(if clauses (do (list (quote if) (first clauses) (if (next clauses) (second clauses) (throw (IllegalArgumentException. cond requires an even number of forms))) (cons (quote clojure.core/cond) (next (next clauses))))))
+            (cons 'clojure.core/cond (next (next clauses)))))        ;(if clauses (do (list (quote if) (first clauses) (if (next clauses) (second clauses) (throw (IllegalArgumentException. cond requires an even number of forms))) (cons (quote clojure.core/cond) (next (next clauses))))))
 
+(defn is-this-number? [n]
+  (when (number? n)
+        (println "Yes it is a number")))
+
+(is-this-number? 4)
 
 (defn this-is-func [x & body]
   (println (list 'if x (cons 'do body))))
